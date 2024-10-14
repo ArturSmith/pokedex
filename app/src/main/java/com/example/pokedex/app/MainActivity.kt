45c90a34172aaae989aa4ef5d00b7f162c24a58c
Arthur Smith <arthur9smith7@gmail.com>
2024-10-14 18:26:48 +0200

    Created structure of project.

--- app/src/main/java/com/example/pokedex/app/MainActivity.kt
+++ app/src/main/java/com/example/pokedex/app/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.pokedex
+package com.example.pokedex.app
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -10,7 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.pokedex.ui.theme.PokedexTheme
+import com.example.pokedex.app.ui.theme.PokedexTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
